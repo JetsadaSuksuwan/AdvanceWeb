ba0a233cd163e065ec897217bedd7ecbc60a5895
JetsadaSuksuwan <jetsada.suksuwan@gmail.com>
2021-03-16 10:11:51 +0700

    ddd

---
+++ Lecture-07/calc.py
@@ -0,0 +1,22 @@
+def add(x,y):
+    """Add Function"""
+    return x + y
+
+
+def subtract(x,y):
+    """subtract Function"""
+    return x - y
+
+
+def multiply(x,y):
+    """multiply Function"""
+    return x * y
+
+
+def divide(x,y):
+    """divide Function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero!!')
+    return x / y
+
+print(divide(10,2))
